c0dcbb4c760ad4675ed891aea39eb43bcab6abe2
iskwon <kisub31@gmail.com>
2014-10-24 11:03:16 +0900

    MenuOriginalDTO.java / menuOriginalSQL.xml 추가

--- src/menu/originalAction.java
+++ src/menu/originalAction.java
@@ -1,5 +1,36 @@
 package menu;
 
-public class originalAction {
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+import com.opensymphony.xwork2.Preparable;
+
+public class OriginalAction extends ActionSupport implements IbatisAware, Preparable, ModelDriven{
+
+	public static SqlMapClient sqlMapper;
+	
+	public String execute() throws Exception{
+		
+		return SUCCESS;
+	}
+	
+	@Override
+	public Object getModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void prepare() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setIbatis(SqlMapClient sqlMapper) {
+		// TODO Auto-generated method stub
+		this.sqlMapper = sqlMapper;
+	}
+	
 
 }
