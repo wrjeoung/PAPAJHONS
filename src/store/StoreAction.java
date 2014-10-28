ac2b2cae23b908b2d45d1cb96fbe52b95115b8ab
iskwon <kisub31@gmail.com>
2014-10-28 18:06:54 +0900

    매장찾기 파일, 이미지 추가

---
+++ src/store/StoreAction.java
@@ -0,0 +1,12 @@
+package store;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class StoreAction extends ActionSupport {
+	
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+	
+
+}
