8ed9ae917b56519af149b025f28039e6d4e28cb2
beomsik <a02110118@gmail.com>
2014-11-03 17:49:12 +0900

    Qna 리스트 1

---
+++ src/qna/board/shoplistAction.java
@@ -0,0 +1,64 @@
+package qna.board;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class shoplistAction extends ActionSupport{
+	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+	
+	private qnaVO vo=new qnaVO();
+	private String qmenu;
+	
+	private List<qnaVO> list4=new ArrayList<qnaVO>();
+	
+	public shoplistAction()throws IOException{
+		reader=Resources.getResourceAsReader("sqlMapConfig.xml");
+		sqlMapper=SqlMapClientBuilder.buildSqlMapClient(reader);
+		reader.close();
+	}
+	public String execute()throws Exception{
+		vo.setQmenu(getQmenu());
+		list4=sqlMapper.queryForList("qna.selectShop", getQmenu());
+		
+		return SUCCESS;
+	}
+	public static Reader getReader() {
+		return reader;
+	}
+	public static void setReader(Reader reader) {
+		shoplistAction.reader = reader;
+	}
+	public static SqlMapClient getSqlMapper() {
+		return sqlMapper;
+	}
+	public static void setSqlMapper(SqlMapClient sqlMapper) {
+		shoplistAction.sqlMapper = sqlMapper;
+	}
+	public qnaVO getVo() {
+		return vo;
+	}
+	public void setVo(qnaVO vo) {
+		this.vo = vo;
+	}
+	public String getQmenu() {
+		return qmenu;
+	}
+	public void setQmenu(String qmenu) {
+		this.qmenu = qmenu;
+	}
+	public List<qnaVO> getList4() {
+		return list4;
+	}
+	public void setList4(List<qnaVO> list4) {
+		this.list4 = list4;
+	}
+
+}
---
+++ src/qna/board/storelistAction.java
@@ -0,0 +1,62 @@
+package qna.board;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class storelistAction extends ActionSupport{
+	public static Reader reader;
+	public static SqlMapClient sqlMapper;
+	
+	private qnaVO vo=new qnaVO();
+	private String qmenu;
+	
+	private List<qnaVO> list7=new ArrayList<qnaVO>();
+	
+	public storelistAction()throws IOException{
+		reader=Resources.getResourceAsReader("sqlMapConfig.xml");
+		sqlMapper=SqlMapClientBuilder.buildSqlMapClient(reader);
+		reader.close();
+	}
+	public String execute()throws Exception{
+		vo.setQmenu(getQmenu());
+		list7=sqlMapper.queryForList("qna.selectStore", getQmenu());
+		return SUCCESS;
+	}
+	public static Reader getReader() {
+		return reader;
+	}
+	public static void setReader(Reader reader) {
+		storelistAction.reader = reader;
+	}
+	public static SqlMapClient getSqlMapper() {
+		return sqlMapper;
+	}
+	public static void setSqlMapper(SqlMapClient sqlMapper) {
+		storelistAction.sqlMapper = sqlMapper;
+	}
+	public qnaVO getVo() {
+		return vo;
+	}
+	public void setVo(qnaVO vo) {
+		this.vo = vo;
+	}
+	public String getQmenu() {
+		return qmenu;
+	}
+	public void setQmenu(String qmenu) {
+		this.qmenu = qmenu;
+	}
+	public List<qnaVO> getList7() {
+		return list7;
+	}
+	public void setList7(List<qnaVO> list7) {
+		this.list7 = list7;
+	}
+}
