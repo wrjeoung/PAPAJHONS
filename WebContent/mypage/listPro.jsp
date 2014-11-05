42a001427810d1332c8d377482576a44a6788a6e
juhyukKim <kimjuggang@nate.com>
2014-11-05 14:42:26 +0900

    mypageList 관련 추가 수정

--- WebContent/mypage/listPro.jsp
+++ WebContent/mypage/listPro.jsp
@@ -2,7 +2,7 @@
     pageEncoding="utf-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <script type="text/javascript">
-	if(parent.$("tbody tr").size() > 1)
+	if(parent.$("tbody tr").size() >= 1)
 	{
 		parent.$("tbody").empty();
 	}
--- src/papa/member/MyOrderAction.java
+++ src/papa/member/MyOrderAction.java
@@ -3,19 +3,22 @@ package papa.member;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
 
 import papa.address.IbatisAware;
 
 import com.ibatis.sqlmap.client.SqlMapClient;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class MyOrderAction extends ActionSupport implements IbatisAware {
+public class MyOrderAction extends ActionSupport implements IbatisAware,SessionAware {
 	public static SqlMapClient sqlMapper;	//SqlMapClient API를 사용하기 위한 sqlMapper 객체.
 	private List<MyOrderData> list = new ArrayList<MyOrderData>();
 	private String sdate;
 	private String edate;
-	private String memId;
 	private String orderList = null;
+	private Map sessionMap;
 		
 	public String getOrderList() {
 		return orderList;
@@ -23,14 +26,6 @@ public class MyOrderAction extends ActionSupport implements IbatisAware {
 
 	public void setOrderList(String orderList) {
 		this.orderList = orderList;
-	}
-
-	public String getMemId() {
-		return memId;
-	}
-
-	public void setMemId(String memId) {
-		this.memId = memId;
 	}
 
 	public String getEdate() {
@@ -56,12 +51,13 @@ public class MyOrderAction extends ActionSupport implements IbatisAware {
 	public String execute() throws Exception {
 		HashMap params = new HashMap();
 		StringBuffer buffer = new StringBuffer();
+		String memId = (String) sessionMap.get("memId");
 		
 		params.put("sdate", sdate);
 		params.put("edate", edate);
 		params.put("memId", memId);
 		
-		
+		System.out.println("memId="+memId);
 		list = sqlMapper.queryForList("myorderSQL.selectBydate",params);
 		
 		for(MyOrderData data : list)
@@ -78,11 +74,10 @@ public class MyOrderAction extends ActionSupport implements IbatisAware {
 			buffer.append("<td>"+status+"</td>");
 			buffer.append("</tr>");
 		}
-		
 		orderList = buffer.toString();
-		if(orderList == null)
+		if(orderList.length() < 1)
 		{
-			orderList = "<tr><td colspan=9>조회된 내용이 없습니다.</td></tr>";
+			orderList = "<tr><td colspan=6>조회된 내용이 없습니다.</td></tr>";
 		}
 	
 		return SUCCESS;
@@ -91,5 +86,10 @@ public class MyOrderAction extends ActionSupport implements IbatisAware {
 	public void setIbatis(SqlMapClient sqlMapper) {
 		// TODO Auto-generated method stub
 		this.sqlMapper = sqlMapper;
+	}
+
+	@Override
+	public void setSession(Map arg0) {
+		sessionMap = arg0;
 	}
 }
