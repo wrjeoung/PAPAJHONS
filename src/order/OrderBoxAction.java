dd21778e47f6f6ef8dd45b03d03bcfdc7165dd40
wrjeoung <wr_jeoung@naver.com>
2014-11-05 10:29:36 +0900

    컴파일에러 수정.

---
+++ src/order/OrderBoxAction.java
@@ -0,0 +1,13 @@
+package order;
+
+import com.opensymphony.xwork2.Action;
+
+public class OrderBoxAction implements Action {
+
+
+	public String execute() throws Exception {
+			
+		return SUCCESS;
+	}
+
+}
