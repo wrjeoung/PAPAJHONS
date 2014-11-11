fa26c48334ebbf86ff565369770e075b048c643d
wrjeoung <wr_jeoung@naver.com>
2014-11-11 11:16:08 +0900

    주문하기 action 연결.

--- src/order/OrderListAction.java
+++ src/order/OrderListAction.java
@@ -1,19 +1,21 @@
 package order;
 
+import com.ibatis.sqlmap.client.SqlMapClient;
 import com.opensymphony.xwork2.Action;
 
-public class OrderListAction implements Action {
+public class OrderListAction implements Action, IbatisAware {
+	public static SqlMapClient sqlMapper;
+	
 
-	@Override
 	public String execute() throws Exception {
-		
-		
-		
-		
-		
 		
 
 		return SUCCESS;
+	}
+
+
+	public void setIbatis(SqlMapClient sqlMapper) {
+		this.sqlMapper = sqlMapper;
 	}
 
 }
